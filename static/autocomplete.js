325f809cd4a8c22fae64370f07857bf9100b9f98
Pooja <77185427+pooja610@users.noreply.github.com>
2022-05-28 00:25:38 +0530

    Updated autocomplete.js

--- static/autocomplete.js
+++ static/autocomplete.js
@@ -1,12 +1,11 @@
 new autoComplete({
-    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+    data: {                              // Data src [Array, Function, Async] 
       src: films,
     },
-    selector: "#autoComplete",           // Input field selector              | (Optional)
-    threshold: 2,                        // Min. Chars length to start Engine | (Optional)
-    debounce: 100,                       // Post duration for engine to start | (Optional)
-    searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-    resultsList: {                       // Rendered results list object      | (Optional)
+    selector: "#autoComplete",           // Input field selector              
+    threshold: 0,                        // Min. Chars length-->start engine as soon as the user types 
+    searchEngine: "strict",              // Search Engine mode         
+    resultsList: {                       // Rendered results list object      
         render: true,
         container: source => {
             source.setAttribute("id", "food_list");
@@ -15,22 +14,22 @@ new autoComplete({
         position: "afterend",
         element: "ul"
     },
-    maxResults: 5,                         // Max. number of rendered results | (Optional)
-    highlight: true,                       // Highlight matching results      | (Optional)
-    resultItem: {                          // Rendered result item            | (Optional)
+    maxResults: 5,                         // Max. number of rendered results to show up while searching
+    highlight: true,                       // Highlight matching results in search    
+    resultItem: {                          // Rendered result item           
         content: (data, source) => {
             source.innerHTML = data.match;
         },
         element: "li"
     },
-    noResults: () => {                     // Action script on noResults      | (Optional)
+    noResults: () => {                     // Script for noResults     
         const result = document.createElement("li");
         result.setAttribute("class", "no_result");
         result.setAttribute("tabindex", "1");
         result.innerHTML = "No Results";
         document.querySelector("#autoComplete_list").appendChild(result);
     },
-    onSelection: feedback => {             // Action script onSelection event | (Optional)
+    onSelection: feedback => {             // Script for Selection event
         document.getElementById('autoComplete').value = feedback.selection.value;
     }
 });
